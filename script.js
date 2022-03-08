0be61c5c13873365f73068ff72f25bf118980c03
felix1805 <felix1805@gmail.com>
2022-03-08 14:17:18 -0500

    added functions for initial city input and control

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 var key = a12e022cb62b59c204d6c4c7065d99c2
+var cities = $["cities"];
+var city = [];
+
+function storedCit () {
+var cityStorage = JSON.parse(localStorage.getItem('city'));
+if (cityStorage != NaN){
+    city = cityStorage;
+}
+//displayCity();
+//storedCit();
+
+}
+
+function localStorageCities(){
+    localStorage.setItem('city', JSON.stringify(city));
+}
+
+function displayCity(){
+    cities.innerHTML = '';
+    for (let i = 0; i < city.length; i++) {
+        var cityindex = city[i];
+
+        var li = $('<li>').text(cityindex);
+        cities.prepend(li);
+        li.attr('cityinfo', cityindex);
+        li.attr('class', "list-group-item");
+        li.attr('id','citylist');
+        
+    }
+    if (!cityindex){
+        return
+    } else{
+        weatherInfo(cityindex)
+    };
+}
+
+$('#querycity').on('click', function(event) {
+    event.preventDefault();
+    var cityindex = $('#cityid').val().trim();
+
+})
